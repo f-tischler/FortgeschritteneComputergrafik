b395abe2d1f03e74abb7894e0d10651e3f289f3b
Florian Tischler <Florian Tischler>
2016-02-01 02:14:28 +0100

    fixed camera look at presentation update

--- ACG-Assignment3/src/main.cpp
+++ ACG-Assignment3/src/main.cpp
@@ -20,15 +20,15 @@ int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 
-	constexpr auto width = 1024ul;
-	constexpr auto height = 768ul;
+	constexpr auto width = 640ul;
+	constexpr auto height = 480ul;
 
 	constexpr auto roomWidth  = 400.f;
 	constexpr auto roomHeight = 300.f;
 
 	const auto camPos = Vector(0, 130.0, 500);
-	const auto lookAt = Vector(0, 140, 0);
-	constexpr auto fov = 80.0f / 180.0f * PI;
+	const auto lookAt = Vector(0, 145, 190);
+	constexpr auto fov = 80.0f / 160.0f * PI;
 
 	constexpr auto lightPower = 1000000.f;
 
@@ -58,6 +58,9 @@ int main(int argc, char* argv[])
 
 	//front left
 	scene.AddGeometry(std::make_unique<Sphere>(greySpecularMat, Vector(-100, 0, 280), 50.0f));
+
+	// camera focus point
+	scene.AddGeometry(std::make_unique<Sphere>(whiteDiffuseMat, lookAt, 5.0f));
 
 	//front right
 	//scene.AddGeometry(std::make_unique<Sphere>(whiteTransMat, Vector(75, 60, 250), 40.0f));
@@ -103,9 +106,9 @@ int main(int argc, char* argv[])
 		auto config = RaycasterConfiguration
 		{
 			2, // subSamplesPerPixel
-			32, // unsigned int samplesPerSubSample;
+			2, // unsigned int samplesPerSubSample;
 			1, // unsigned int dofSamples;
-			2, // float apetureSize;
+			4, // float apetureSize;
 		};
 
 		auto raycaster = Raycaster<decltype(radianceProvider)>(image, camera, radianceProvider, config);
